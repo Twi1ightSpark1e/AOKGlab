1fee6e156ce3c105c1ff8a2fa46e2d4edd889797
daniilak <daniilakk@gmail.com>
2017-10-07 21:27:19 +0300

    added indices for Lab4, may be crazy:D

--- UniverGraphics/MainForm.cs
+++ UniverGraphics/MainForm.cs
@@ -307,51 +307,47 @@ namespace UniverGraphics
         };
         public static ModelPoint[] points = new ModelPoint[]
         {
-            //нижняя панель
-            new ModelPoint((-1, 0, -1), (1, 1, 1)),
-            new ModelPoint((-1, 0, 1), (1, 1, 1)),
-            new ModelPoint((1, 0, -1), (1, 1, 1)),
-            new ModelPoint((1, 0, 1), (1, 1, 1)),
-            //правая панель
-            new ModelPoint((1, 0, 1), (1, 0, 0)),
-            new ModelPoint((1, 0, -1), (1, 0, 0)),
-            new ModelPoint((1, 2, 1), (1, 0, 0)),
-            new ModelPoint((1, 2, 1), (1, 0, 0)),
-            new ModelPoint((1, 0, -1), (1, 0, 0)),
-            new ModelPoint((1, 2, -1), (1, 0, 0)),
-            //левая панель
-            new ModelPoint((-1, 0, -1), (0, 1, 0)),
-            new ModelPoint((-1, 0, 1), (0, 1, 0)),
-            new ModelPoint((-1, 2, -1), (0, 1, 0)),
-            new ModelPoint((-1, 2, -1), (0, 1, 0)),
-            new ModelPoint((-1, 0, 1), (0, 1, 0)),
-            new ModelPoint((-1, 2, 1), (0, 1, 0)),
-            //верхняя панель
-            new ModelPoint((-1, 2, -1), (0, 0, 1)),
-            new ModelPoint((-1, 2, 1), (0, 0, 1)),
-            new ModelPoint((1, 2, -1), (0, 0, 1)),
-            new ModelPoint((1, 2, -1), (0, 0, 1)),
-            new ModelPoint((-1, 2, 1), (0, 0, 1)),
-            new ModelPoint((1, 2, 1), (0, 0, 1)),
-            //передняя панель
-            new ModelPoint((-1, 2, 1), (1, 1, 0)),
-            new ModelPoint((-1, 0, 1), (1, 1, 0)),
-            new ModelPoint((1, 0, 1), (1, 1, 0)),
-            new ModelPoint((-1, 2, 1), (1, 1, 0)),
-            new ModelPoint((1, 0, 1), (1, 1, 0)),
-            new ModelPoint((1, 2, 1), (1, 1, 0)),
-            //задняя панель
-            new ModelPoint((-1, 0, -1), (1, 0, 1)),
-            new ModelPoint((1, 0, -1), (1, 0, 1)),
-            new ModelPoint((1, 2, -1), (1, 0, 1)),
-            new ModelPoint((-1, 2, -1), (1, 0, 1)),
-            new ModelPoint((-1, 0, -1), (1, 0, 1)),
-            new ModelPoint((1, 2, -1), (1, 0, 1)),
+             //front
+            new ModelPoint((-1, -1 , -1), (255, 255, 255)),
+            new ModelPoint((1, -1, 1), (255, 255, 255)),
+            new ModelPoint((1, 1, 1), (255, 255, 255)),
+            new ModelPoint((-1, 1, -1), (255, 255, 255)),
+            //right
+            new ModelPoint((1, 1, 1), (255, 0, 0)),
+            new ModelPoint((1, 1, -1), (255, 0, 0)),
+            new ModelPoint((1, -1, -1), (255, 0, 0)),
+            new ModelPoint((1, -1, 1), (255, 0, 0)),
+            
+            //back
+            new ModelPoint((-1, -1, -1), (0, 255, 0)),
+            new ModelPoint((1, -1, -1), (0, 255, 0)),
+            new ModelPoint(( 1, 1, -1), (0, 255, 0)),
+            new ModelPoint((-1, 1, -1), (0, 255, 0)),
+            //left
+            new ModelPoint((-1, -1, -1), (0, 0, 255)),
+            new ModelPoint((-1, -1, 1), (0, 0, 255)),
+            new ModelPoint((-1, 1, 1), (0, 0, 255)),
+            new ModelPoint((-1, 1, -1), (0, 0, 255)),
+            //upper
+            new ModelPoint(( 1, 1, 1), (255, 255, 0)),
+            new ModelPoint((-1, 1, 1), (255, 255, 0)),
+            new ModelPoint((-1, 1, -1), (255, 255, 0)),
+            new ModelPoint((1, 1, -1), (255, 255, 0)),
+            //bottom
+            new ModelPoint((-1, -1, -1), (255, 0, 255)),
+            new ModelPoint((1, -1, -1), (255, 0, 255)),
+            new ModelPoint(( 1, -1, 1), (255, 0, 255)),
+            new ModelPoint((-1, -1, 1), (255, 0, 255)),
         };
 
         public static uint[] indices = new uint[]
         {
-            0, 1, 2, 2, 1, 3, //нижняя панель
+            0,  1,  2,  0,  2,  3,   //front
+            4,  5,  6,  4,  6,  7,   //right
+            8,  9,  10, 8,  10, 11,  //back
+            12, 13, 14, 12, 14, 15,  //left
+            16, 17, 18, 16, 18, 19,  //upper
+            20, 21, 22, 20, 22, 23   //bottom
 
         };
 
